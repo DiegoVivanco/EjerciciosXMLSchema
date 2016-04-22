43d596f39070b000977f542f1d9949b0a1448b8a
Diego Leonardo Vivanco Moreira <leox_vm@hotmail.com>
2016-04-22 10:19:37 +0200

    Update SalidaObjeto_biciPalma.java

--- JavaSchemas_JAXB/src/biciPalma_JAXB/SalidaObjeto_biciPalma.java
+++ JavaSchemas_JAXB/src/biciPalma_JAXB/SalidaObjeto_biciPalma.java
@@ -10,14 +10,37 @@ public class SalidaObjeto_biciPalma {
 		try {
 			final JAXBContext jc = JAXBContext.newInstance(Estacion.class.getPackage().getName());
 			final Unmarshaller u = jc.createUnmarshaller();
-	        final JAXBElement<Estacion> menuElement = (JAXBElement<Estacion>)u.unmarshal(SalidaObjeto_biciPalma.class.getResourceAsStream("/estacion.xml"));
+	                final JAXBElement<Estacion> menuElement = (JAXBElement<Estacion>)u.unmarshal(SalidaObjeto_biciPalma.class.getResourceAsStream("/estacion.xml"));
 			System.out.println("Estacion: " + menuElement.getValue().getId());
 			System.out.println("Direccion: " + menuElement.getValue().getDireccion());
 			System.out.println("Numero de anclajes: " + menuElement.getValue().getNumeroAnclajes());
-			System.out.println("Bicis y anclajes: " + menuElement.getValue().getAnclajes());
-
+			
+			System.out.println("Bicis y anclajes:");
+			consultarAnclajes(menuElement);
+			
 		} catch (JAXBException e) {
 			e.printStackTrace();
+			
+		public static void consultarAnclajes(JAXBElement<Estacion> menuElement){
+			// recorre el array anclajes y muestra el id de la bici anclada o si estÃ¡ libre
+			
+			// la clase Bicicleta no es creada por JAXB porque he declarado el tipo del elemento bicicleta
+			// como simpleContent restriction de un string en el Schema.
+			// La clase Anclajes es un List<String> bicicleta
+		
+			int posicion = 0;
+			int numeroAnclaje = 0;
+			
+			for(String bicicleta:(menuElement.getValue().getAnclajes()).getBicicleta() ){
+				numeroAnclaje = posicion + 1;
+				//System.out.println(bicicleta);
+				if( bicicleta.matches("[0-9]{3}") ){
+					System.out.println("Anclaje " + numeroAnclaje + " : " + bicicleta);
+				}
+				else
+					System.out.println("Anclaje " + numeroAnclaje + " : " + " libre");
+				
+				posicion++;	
 		}
 	}
 }
